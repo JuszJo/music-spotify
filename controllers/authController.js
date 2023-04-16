2da697b2c40fc2ed1f06790c54a7f95ce1e20e7b
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-04-16 15:26:15 +0100

    implement auth service

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,36 +1,8 @@
-const {MongoClient} = require('mongodb')
-
-const mongoURI = `mongodb+srv://${process.env.NAME}:${process.env.PASSWD}@cluster0.xjoqb.mongodb.net`
-const client = new MongoClient(mongoURI);
-
-function validateUser(result, password) {
-    return result.password == password ? true : false;
-}
-
-async function getUser(username, password) {
-    try {
-        await client.connect();
-        await client.db("spotify").command({ping: 1})
-        console.log("Connected")
-        
-        let result = await client.db("spotify").collection("users").findOne({
-            username: username,
-        })
-
-        return result == null ? false : validateUser(result, password);
-    } 
-    catch(err) {
-        if(err) console.error("Something went wrong!", err);
-    }
-    finally {
-        console.log("Connection Closed")
-        await client.close();
-    }
-}
+const authService = require('../services/auth.service');
 
 exports.loginHandler = async (req, res, next) => {
     try {
-        if(!(await getUser(req.body.username, req.body.password))) res.redirect('/login')
+        if(!(await authService.getUser(req.body.username, req.body.password))) res.redirect('/login')
         else {
             req.session.user = req.body.username;
             res.redirect('/welcome')
@@ -40,4 +12,3 @@ exports.loginHandler = async (req, res, next) => {
         if(err) next(err);
     }
 }
-
---
+++ services/auth.service.js
@@ -0,0 +1,29 @@
+const {MongoClient} = require('mongodb')
+
+const mongoURI = `mongodb+srv://${process.env.NAME}:${process.env.PASSWD}@cluster0.xjoqb.mongodb.net`
+const client = new MongoClient(mongoURI);
+
+function validateUser(result, password) {
+    return result.password == password ? true : false;
+}
+
+exports.getUser = async (username, password) => {
+    try {
+        await client.connect();
+        await client.db("spotify").command({ping: 1})
+        console.log("Connected")
+        
+        let result = await client.db("spotify").collection("users").findOne({
+            username: username,
+        })
+
+        return result == null ? false : validateUser(result, password);
+    } 
+    catch(err) {
+        if(err) console.error("Something went wrong!", err);
+    }
+    finally {
+        console.log("Connection Closed")
+        await client.close();
+    }
+}
