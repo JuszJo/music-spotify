57698ad3dc387cea1bebba0f1b4b276a087b6cc7
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-05-03 16:03:19 +0100

    update util function

--- utils/makeParagraph.utils.js
+++ utils/makeParagraph.utils.js
@@ -1,5 +1,5 @@
-export default function makeParagraph(text, paragraphAmount) {
-    let points = text.length / paragraphAmount;   
+export default function makeParagraph(text) {
+    let points = text.length / 5;   
     let array = [];
 
     for(let i = 0; i < text.length; i += points) array.push(String(text).slice(i, i + points))
